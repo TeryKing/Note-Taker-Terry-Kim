e0382a61cd213a75226e4564b196c2fe4531e16d
TeryKing <tery_x3@hotmail.com>
2022-09-18 03:14:38 -0500

    port active, starter code visually works, function still failing atm

--- server.js
+++ server.js
@@ -3,4 +3,35 @@ const path = require("path");
 const port = process.env.PORT||3001;
 const express = require("express");
 const app = express();
+const notes = require("./db/db.json");
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get("/api/notes", (req,res)=>{
+    res.json(notes.slice(1));
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(true);
+});
+
+app.listen(port, () => {
+    console.log(`Active port ${port}!`);
+});
 
