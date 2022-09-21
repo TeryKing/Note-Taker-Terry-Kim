4f3f33d92e2c83914ae305fecacd360ef0ce5c12
TeryKing <tery_x3@hotmail.com>
2022-09-20 19:32:57 -0500

    added the ADDING and Deleting Function

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 
+app.get("/api/notes", (req,res)=>{
+    res.json(notes.slice(1));
+});
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -21,17 +24,47 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get("/api/notes", (req,res)=>{
-    res.json(notes.slice(1));
+
+function create(body, notearray){
+    const newNote = body;
+    if(!Array.isArray(notearray))
+        notearray =[];
+    
+    if(notearray.length === 0)
+            notearray.push(0);
+    body.id = notearray[0];
+    notearray[0]++;
+
+    notearray.push(newNote);
+    fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(notearray, null, 2));
+
+    return newNote;
+
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = create(req.body, notes);
+    res.json(newNote);
 });
 
+function deleteit(id, notearray){
+    for(let i =0; i<notearray.length;i++){
+        let note = notearray[1];
+
+        if(note.id == id){
+            notearray.splice(i,1);
+            fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(notearray,null,2));
+        break;
+        }
+    }
+}
 
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, notes);
+    deleteit(req.params.id, notes);
     res.json(true);
 });
 
-app.listen(port, () => {
-    console.log(`Active port ${port}!`);
-});
 
+app.listen(port, () => {
+    console.log(`Active port on ${port}!`);
+});
